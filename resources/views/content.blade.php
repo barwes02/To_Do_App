bc45a8f7f19b7c7d955b1eed604d0c7f0ffabbdb
barwes02 <sbarwellsmith@gmail.com>
2020-08-07 23:50:58 +1200

    Initial Commit

---
+++ resources/views/content.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+@section('content')
+<div class="wrapper">
+    <div>
+        <h1 class="text-white display-4 headingText">To Do App</h1>
+        <todo-component></todo-component>
+    </div>
+</div>
+@endsection
